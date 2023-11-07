d873faba61a75c2c5de9dace847d834cb6b0b071
Avneet-s1b <avneet.bhatia@berylsystems.com>
2023-11-07 17:52:08 +0530

    Adds sol to find longest word problem

--- Ruby/Practice/longest_word.rb
+++ Ruby/Practice/longest_word.rb
@@ -1,13 +1,12 @@
 str = "This is a typical sentence"
 
-ans = ""
-
 words = str.split(" ")
+max = ""
 for i in 0..words.length-1
     word = words[i]
-    if word.length>=5
-        words[i] = word.reverse
+    if word.length>max.length
+        max = word
     end
 end
 
-puts words.join(" ")
+puts max
