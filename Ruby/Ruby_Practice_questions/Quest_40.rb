7cbe340cd9bc52d92902b64524bd97c46c5b07a5
Avneet-s1b <avneet.bhatia@berylsystems.com>
2023-11-16 16:32:38 +0530

    Adds sol of Q40 of ruby practice sheet

---
+++ Ruby/Ruby_Practice_questions/Quest_40.rb
@@ -0,0 +1,13 @@
+def fun(year)
+    count = 0
+    Date.new(year).step(Date.new(year,12,31)){|date| count += 1 if date.wday == 6 && date.day == 14}
+    return count
+end
+
+require 'date'
+
+print "Enter year: "
+
+year = gets.chomp.to_i
+
+puts fun year
