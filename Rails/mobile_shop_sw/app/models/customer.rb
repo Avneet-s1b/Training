07714646792eeec33123d08499c8fef7f90fe4a1
Avneet-s1b <avneet.bhatia@berylsystems.com>
2023-11-24 17:50:50 +0530

    Adds custom validator and custom methods to mobile_shop_sw

--- Rails/mobile_shop_sw/app/models/customer.rb
+++ Rails/mobile_shop_sw/app/models/customer.rb
@@ -1,11 +1,23 @@
+class MyValidator < ActiveModel::Validator
+    def validate(record)
+        unless record.name.start_with? 'X'
+            record.errors.add :name, "Provide a name starting with X, please!"
+        end
+    end
+end
+
 class Customer < ApplicationRecord
     # validates :name, presence: {strict:true} , length: { is: 6 }
     # validates :end, comparison: { greater_than: :start }
     # validates :name, inclusion: { in: ['Action', 'Science Fiction', 'Drama', 'Horror', 'Comedy', 'Musical'] }, allow_nil: true
     # validates :end, numericality: {only_integer:true, strict:true}
-    validates :adhar_number, presence: true, unless: Proc.new{|obj| obj.mobile.empty?}
-
+    # validates :adhar_number, presence: true, unless: Proc.new{|obj| obj.mobile.empty?}
+    # validates :adhar_number, presence: true, unless: Proc.new{|obj| obj.mobile.empty?}
+    
+    
     # def mobile_pres
-    #     mobile.empty?
+    #     mobile.empty? == false
     # end
+
+    validates_with MyValidator
 end
