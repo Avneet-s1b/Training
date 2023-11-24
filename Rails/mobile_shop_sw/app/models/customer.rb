fc89b4fbc5afb2cb32713c97cc5ed87f23b0c1cb
Avneet-s1b <avneet.bhatia@berylsystems.com>
2023-11-24 15:55:01 +0530

    Adds validation options(allow_nil,strict,message) to rails.txt

--- Rails/mobile_shop_sw/app/models/customer.rb
+++ Rails/mobile_shop_sw/app/models/customer.rb
@@ -1,6 +1,6 @@
 class Customer < ApplicationRecord
-    # validates :name, presence: true , length: { is: 6 }
+    # validates :name, presence: {strict:true} , length: { is: 6 }
     # validates :end, comparison: { greater_than: :start }
-    #  validates :name, inclusion: { in: ['Action', 'Science Fiction', 'Drama', 'Horror', 'Comedy', 'Musical'] }
-    validates :end, numericality: {only_integer:true}
+    validates :name, inclusion: { in: ['Action', 'Science Fiction', 'Drama', 'Horror', 'Comedy', 'Musical'] }, allow_nil: true
+    # validates :end, numericality: {only_integer:true, strict:true}
 end
