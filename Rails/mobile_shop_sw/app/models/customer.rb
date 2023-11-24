0d82154581aedd301e9112dad1560591c3c094ff
Avneet-s1b <avneet.bhatia@berylsystems.com>
2023-11-24 16:52:57 +0530

    Adds Conditional validations to rails.txt as well as mobile_shop_sw

--- Rails/mobile_shop_sw/app/models/customer.rb
+++ Rails/mobile_shop_sw/app/models/customer.rb
@@ -1,6 +1,11 @@
 class Customer < ApplicationRecord
     # validates :name, presence: {strict:true} , length: { is: 6 }
     # validates :end, comparison: { greater_than: :start }
-    validates :name, inclusion: { in: ['Action', 'Science Fiction', 'Drama', 'Horror', 'Comedy', 'Musical'] }, allow_nil: true
+    # validates :name, inclusion: { in: ['Action', 'Science Fiction', 'Drama', 'Horror', 'Comedy', 'Musical'] }, allow_nil: true
     # validates :end, numericality: {only_integer:true, strict:true}
+    validates :adhar_number, presence: true, unless: Proc.new{|obj| obj.mobile.empty?}
+
+    # def mobile_pres
+    #     mobile.empty?
+    # end
 end
