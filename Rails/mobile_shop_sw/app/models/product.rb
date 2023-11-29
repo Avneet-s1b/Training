776b3d6bcdde56871b5c531a6cfea272a30fa814
Avneet-s1b <avneet.bhatia@berylsystems.com>
2023-11-29 13:30:26 +0530

    Adds after_create callback using methods to Products table

--- Rails/mobile_shop_sw/app/models/product.rb
+++ Rails/mobile_shop_sw/app/models/product.rb
@@ -1,8 +1,16 @@
 class Product < ApplicationRecord
+
+    after_create :creation
+
     before_create do
         puts "Creating an object in Products table"
     end
-    # after_initialize do |product|
-    #     puts "You have initialized an object!"
-    # end
+    after_initialize do |user|
+        puts "You have initialized a product object!"
+    end
+
+    private
+        def creation
+            puts "Product added successfully"            
+        end
 end
