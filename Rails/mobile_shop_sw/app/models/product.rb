a4de16e37adad5a44295085a36802d7d01035848
Avneet-s1b <avneet.bhatia@berylsystems.com>
2023-11-29 13:44:09 +0530

    Adds after_initialize using proc callback to products table

--- Rails/mobile_shop_sw/app/models/product.rb
+++ Rails/mobile_shop_sw/app/models/product.rb
@@ -2,12 +2,15 @@ class Product < ApplicationRecord
 
     after_create :creation
 
-    before_create do
-        puts "Creating an object in Products table"
-    end
-    after_initialize do |user|
-        puts "You have initialized a product object!"
-    end
+    after_validation :creation
+
+    # before_create do
+    #     puts "Creating an object in Products table"
+    # end
+    after_initialize ->(user){puts "You have initialized a product object!"}
+    # after_initialize do |user|
+    #     puts "You have initialized a product object!"
+    # end
 
     private
         def creation
