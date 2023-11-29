3b245979cdf0379a1d1a948d1cbf59a6911c0e13
Avneet-s1b <avneet.bhatia@berylsystems.com>
2023-11-29 13:57:48 +0530

    Adds before_validation callback to products table

--- Rails/mobile_shop_sw/app/models/product.rb
+++ Rails/mobile_shop_sw/app/models/product.rb
@@ -1,19 +1,28 @@
 class Product < ApplicationRecord
 
-    after_create :creation
+    # after_create :creation
 
-    after_validation :creation
+    # after_validation :creation
 
-    # before_create do
-    #     puts "Creating an object in Products table"
-    # end
-    after_initialize ->(user){puts "You have initialized a product object!"}
-    # after_initialize do |user|
-    #     puts "You have initialized a product object!"
-    # end
+    # # before_create do
+    # #     puts "Creating an object in Products table"
+    # # end
+    # after_initialize ->(user){puts "You have initialized a product object!"}
+    # # after_initialize do |user|
+    # #     puts "You have initialized a product object!"
+    # # end
+
+    # private
+    #     def creation
+    #         puts "Product added successfully"            
+    #     end
+
+    attr_accessor :name
+
+    before_validation :remove_whitespaces
 
     private
-        def creation
-            puts "Product added successfully"            
+        def remove_whitespaces
+            name.strip!
         end
 end
