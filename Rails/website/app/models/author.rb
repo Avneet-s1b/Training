5349dae4cc39470a560d293b467f591b20cdb40f
Avneet-s1b <avneet.bhatia@berylsystems.com>
2023-11-30 17:25:24 +0530

    Adds Association callback notes and example to website project

--- Rails/website/app/models/author.rb
+++ Rails/website/app/models/author.rb
@@ -1,3 +1,9 @@
 class Author < ApplicationRecord
-    has_many :books, dependent: :destroy, -> { order "id DESC" }
+    has_many :books, before_add: :check_credit_limit
+
+    def check_credit_limit(book)
+        puts "Adds a booooooooook!"
+    end
+
+
 end
