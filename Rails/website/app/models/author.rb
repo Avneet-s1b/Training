eb3279f8c8621a7613c29a544f06d532ab472cc4
Avneet-s1b <avneet.bhatia@berylsystems.com>
2023-12-05 13:36:24 +0530

    Adds block,inline,class,id,iframes,script,responsiveness notes to html.txt along with examples

--- Rails/website/app/models/author.rb
+++ Rails/website/app/models/author.rb
@@ -1,9 +1,7 @@
 class Author < ApplicationRecord
-    has_many :books, before_add: :check_credit_limit
+  has_many :books, before_add: :check_credit_limit
 
-    def check_credit_limit(book)
-        puts "Adds a booooooooook!"
-    end
-
-
+  def check_credit_limit(book)
+    puts "Adds a booooooooook!"
+  end
 end
--- Rails/website/app/models/entry.rb
+++ Rails/website/app/models/entry.rb
@@ -1,11 +1,11 @@
 module Entryable
-    extend ActiveSupport::Concern
-  
-    included do
-      has_one :entry, as: :entryable, touch: true
-    end
+  extend ActiveSupport::Concern
+
+  included do
+    has_one :entry, as: :entryable, touch: true
+  end
 end
 
 class Entry < ApplicationRecord
-    delegated_type :entryable, types: %w[ Message Comment ], dependent: :destroy
+  delegated_type :entryable, types: %w[ Message Comment ], dependent: :destroy
 end
