ea1e4b1d1d6db44a23b04c72018f07df7f7d2384
Avneet-s1b <avneet.bhatia@berylsystems.com>
2023-12-13 17:06:49 +0530

    Adds code for mail with attachments

--- Rails/website/app/mailers/user_mailer.rb
+++ Rails/website/app/mailers/user_mailer.rb
@@ -4,6 +4,7 @@ class UserMailer < ApplicationMailer
     def welcome_email
         @user = params[:user]
         @url  = 'http://example.com/login'
+        attachments.inline['pic1.png'] = File.read('/home/beryl/Documents/Training/Rails/website/app/assets/pic1.jpg')
         mail(to: @user.email, subject: 'Welcome to My Web Site')
     end
 end
