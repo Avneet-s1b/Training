8a495b0945d96540cc47b5718e7460aad8c6a576
Avneet-s1b <avneet.bhatia@berylsystems.com>
2023-12-14 15:33:25 +0530

    Adds code for sending mail with names in website project

--- Rails/website/app/mailers/user_mailer.rb
+++ Rails/website/app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ApplicationMailer
-    default from: 'avneet.bhatia@berylsystems.com'
-
+    # default from: 'avneet.bhatia@berylsystems.com'
+    default from: email_address_with_name('avneet.bhatia@berylsystems.com', 'Balwinder Singh')
     def welcome_email
         @user = params[:user]
         @url  = 'http://example.com/login'
         attachments.inline['pic1.png'] = File.read('/home/beryl/Documents/Training/Rails/website/app/assets/pic1.jpg')
-        mail(to: @user.email, subject: 'Welcome to My Web Site')
+        mail(to: email_address_with_name(@user.email,@user.name), subject: 'Welcome to My Web Site')
     end
 end
