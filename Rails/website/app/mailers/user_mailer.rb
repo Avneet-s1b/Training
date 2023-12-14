77c5e269d3c5d213b64407b8f3944d289d43a4ef
Avneet-s1b <avneet.bhatia@berylsystems.com>
2023-12-14 16:13:35 +0530

    Adds callbacks in mails in website project

--- Rails/website/app/mailers/user_mailer.rb
+++ Rails/website/app/mailers/user_mailer.rb
@@ -1,10 +1,18 @@
 class UserMailer < ApplicationMailer
     # default from: 'avneet.bhatia@berylsystems.com'
     default from: email_address_with_name('avneet.bhatia@berylsystems.com', 'Balwinder Singh')
+    before_action :set_name_to
+    $name = "J"
     def welcome_email
+        
         @user = params[:user]
         @url  = 'http://example.com/login'
         attachments.inline['pic1.png'] = File.read('/home/beryl/Documents/Training/Rails/website/app/assets/pic1.jpg')
-        mail(to: email_address_with_name(@user.email,@user.name), subject: 'Welcome to My Web Site')
+        mail(to: email_address_with_name(@user.email,@user.name), subject: "Welcome #{$name}to My Web Site")
+    end
+
+    private 
+    def set_name_to
+        $name = "aa"
     end
 end
