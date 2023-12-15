269eb1038937b89449ed6ca985b568fccff43922
Avneet-s1b <avneet.bhatia@berylsystems.com>
2023-12-15 13:00:08 +0530

    Adds authenticate user before performing any action

--- Rails/mobile_shop_sw/app/controllers/users_controller.rb
+++ Rails/mobile_shop_sw/app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update destroy]
-  before_action :authenticate_user!
+  before_action :authenticate_user! , except: %i[index  show]
 
   def index
     @users = User.all
