6f62ad0f354243933b3aad3e72def896721c8610
Avneet-s1b <avneet.bhatia@berylsystems.com>
2023-12-20 18:58:02 +0530

    Adds record not found error handling in my_api

--- Rails/my_api/app/controllers/api/v1/articles_controller.rb
+++ Rails/my_api/app/controllers/api/v1/articles_controller.rb
@@ -2,6 +2,7 @@ module Api
   module V1
     class ArticlesController < ApplicationController
       before_action :set_article, only: %i[ show update destroy ]
+      rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
       # GET /articles
       def index
@@ -52,6 +53,10 @@ module Api
       # Only allow a list of trusted parameters through.
       def article_params
         params.require(:article).permit(:title, :body)
+      end
+
+      def record_not_found(e)
+        render json:{ errors: e }, status: :unprocessable_entity
       end
     end
   end
